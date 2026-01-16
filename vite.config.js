428d0b001069d0df77f2c985af3c14ee4a3e2419
K-Siim <kevinsiim007@gmail.com>
2026-01-16 17:01:58 +0200

    Feature: Downloaded the Laravel Breeze and made it work somehow. Now we need to change the pages visual to our Figma. Same goes to Login/Register and etc.

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,20 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
-        vue(),
-        tailwindcss(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: {
-                name: 'FaunaFond',
-                short_name: 'FaunaFond',
-                start_url: '/',
-                display: 'standalone',
-                background_color: '#ffffff',
-                theme_color: '#2f855a',
-            }
-        })
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-})
+});
