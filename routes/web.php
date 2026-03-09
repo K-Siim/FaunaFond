e0f131d455ce72ccdd6c877349a49c5f0bdcf932
Sorel-Haller <hallersorel@gmail.com>
2026-03-09 16:41:41 +0200

    updated web.php file with vaccines and medications routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PetController;
 use App\Http\Controllers\VetVisitController;
+use App\Http\Controllers\VaccineController;
+use App\Http\Controllers\MedicationController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -33,6 +35,16 @@ Route::middleware('auth')->group(function () {
     Route::post('/pets/{pet}/vet-visits', [VetVisitController::class, 'store'])->name('vet-visits.store');
     Route::put('/pets/{pet}/vet-visits/{vetVisit}', [VetVisitController::class, 'update'])->name('vet-visits.update');
     Route::delete('/pets/{pet}/vet-visits/{vetVisit}', [VetVisitController::class, 'destroy'])->name('vet-visits.destroy');
+
+    // Vaccines routes
+    Route::post('/vaccines',            [VaccineController::class, 'store'])   ->name('vaccines.store');
+    Route::put('/vaccines/{vaccine}',   [VaccineController::class, 'update'])  ->name('vaccines.update');
+    Route::delete('/vaccines/{vaccine}',[VaccineController::class, 'destroy']) ->name('vaccines.destroy');
+    
+    // Medications routes
+    Route::post('/medications',               [MedicationController::class, 'store'])   ->name('medications.store');
+    Route::put('/medications/{medication}',   [MedicationController::class, 'update'])  ->name('medications.update');
+    Route::delete('/medications/{medication}',[MedicationController::class, 'destroy']) ->name('medications.destroy');
 });
 
 require __DIR__.'/auth.php';
