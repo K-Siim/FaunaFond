a6fa340944523cd29c40a27dbf38ba1657babdc0
K-Siim <kevinsiim007@gmail.com>
2026-03-13 11:20:49 +0200

    ADDED profile icon to the other pages components

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -61,6 +61,15 @@ const props = defineProps({
                     </div>
                 </div>
             </transition>
+             <!-- Profiili ikoon -->
+            <Link :href="route('profile.index')" class="absolute top-6 right-6 z-50 p-2"
+                style="color: #275342">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8"
+                    fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                </svg>
+            </Link>
 
             <header class="bg-[#E7F0E4]" v-if="$slots.header">
                 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 pt-20">
