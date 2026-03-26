9bed2adc2a4b7f46ed60233b686f3f8082b19000
K-Siim <kevinsiim007@gmail.com>
2026-03-26 13:31:58 +0200

    Refactor code structure for improved readability and maintainability

--- config/pwa.php
+++ config/pwa.php
@@ -5,22 +5,27 @@ return [
 
     'manifest' => [
         'name' => 'Faunafond',
-        'short_name' => 'LPT',
+        'short_name' => 'Faunafond',
         'background_color' => '#6777ef',
         'display' => 'fullscreen',
         'description' => 'A Progressive Web Application setup for Laravel projects.',
         'theme_color' => '#6777ef',
         'icons' => [
             [
-                'src' => 'Logo.png',
+                'src' => '/images/icons/logo-192.png',
+                'sizes' => '192x192',
+                'type' => 'image/png',
+            ],
+            [
+                'src' => '/images/icons/logo-512.png',
                 'sizes' => '512x512',
-                'type' => 'image/jpg',
+                'type' => 'image/png',
             ],
         ],
     ],
 
     'debug' => env('APP_DEBUG', false), // Show or hide console.log in the browser globally.
-    
-    
+
+
     'livewire-app' => false,
 ];
