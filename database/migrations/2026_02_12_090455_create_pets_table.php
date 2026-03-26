f7b6c51f99c70571477c1a7429691bf0a1f495f2
K-Siim <kevinsiim007@gmail.com>
2026-03-26 11:50:20 +0200

    Create migration for pets table with user_id foreign key

--- database/migrations/2026_02_12_090455_create_pets_table.php
+++ database/migrations/2026_02_12_090455_create_pets_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::dropIfExists('pets');
         Schema::create('pets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
