f9224e2e3c7ea22e3e4869a5a80fa12f439af086
K-Siim <kevinsiim007@gmail.com>
2026-03-26 11:08:43 +0200

    Add user_id foreign key and pet attributes to pets table migration

--- database/migrations/2026_03_26_090455_create_pets_table.php
+++ database/migrations/2026_03_26_090455_create_pets_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('pets', function (Blueprint $table) {
             $table->id();
-            
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('species')->nullable();
             $table->timestamps();
         });
     }
